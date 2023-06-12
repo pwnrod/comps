175bc5130300ec5883b28f0a806000e798d3b14c
Caylin James <caylinjames.dev@gmail.com>
2023-06-12 08:41:25 -0400

    prep the component for using state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 const App = () => {
     const items = [
         {
+            id: "l2kjd",
             label: "Can I use React on a project?",
             content:
                 "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
         },
         {
+            id: "slridj2",
             label: "Can I use JavaScript on a project?",
             content:
                 "You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want.",
         },
         {
+            id: "wjdmco44",
             label: "Can I use TypeScript on a project?",
             content:
                 "You can use TypeScript on any project you want. You can use TypeScript on any project you want. You can use TypeScript on any project you want.",
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,14 @@
 const Accordion = ({ items }) => {
-    return <div>Accordion</div>;
+    const renderedItems = items.map((item) => {
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                <div>{item.content}</div>
+            </div>
+        );
+    });
+
+    return <div>{renderedItems}</div>;
 };
 
 export default Accordion;
