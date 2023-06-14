541d298101cd5b04c79f1e5085bc05a21620e2d3
Caylin James <caylinjames.dev@gmail.com>
2023-06-14 08:49:28 -0400

    implement styling with tailwind

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -12,21 +12,26 @@ const Accordion = ({ items }) => {
         const isExpanded = index === expandedIndex;
 
         const icon = (
-            <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+            <span className="text-xl">
+                {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+            </span>
         );
 
         return (
             <div key={item.id}>
-                <div onClick={() => handleClick(index)}>
-                    {icon}
+                <div
+                    className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+                    onClick={() => handleClick(index)}
+                >
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         );
     });
 
-    return <div>{renderedItems}</div>;
+    return <div className="border-x border-t rounded">{renderedItems}</div>;
 };
 
 export default Accordion;
