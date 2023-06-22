da2ec3e6d1c686e8d6a642c48ffa7b19fc000523
Caylin James <cjames@leapagency.com>
2023-06-22 12:17:56 -0400

    lay foundation for the Navigation context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
 const App = () => {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
-
-    const options = [
-        { label: "Red", value: "red" },
-        { label: "Green", value: "green" },
-        { label: "Blue", value: "blue" },
-    ];
-
-    return (
-        <Dropdown onChange={handleSelect} value={selection} options={options} />
-    );
+    return <div></div>;
 };
 
 export default App;
