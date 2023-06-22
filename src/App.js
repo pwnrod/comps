25ab20a5e752f7e916b13562b3f9c2ec6bbfee39
Caylin James <cjames@leapagency.com>
2023-06-22 13:42:11 -0400

    implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+
 const App = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Link to="/accordion">Go To Accordion</Link>
+            <Link to="/dropdown">Go To Dropdown</Link>
+            <div>
+                <Route path="/accordion">
+                    <AccordionPage />
+                </Route>
+                <Route path="/dropdown">
+                    <DropdownPage />
+                </Route>
+            </div>
+        </div>
+    );
 };
 
 export default App;
