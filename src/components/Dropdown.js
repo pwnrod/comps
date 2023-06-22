2d545d391aa78671377e1f8c513d905323f04a35
Caylin James <cjames@leapagency.com>
2023-06-22 09:25:12 -0400

    add some basic styling with tailwind

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
 const Dropdown = ({ options, value, onChange }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +16,7 @@ const Dropdown = ({ options, value, onChange }) => {
     const renderedOptions = options.map((option) => {
         return (
             <div
+                className="hover:bg-sky-100 rounded cursor-pointer p-1"
                 onClick={() => {
                     handleOptionClick(option);
                 }}
@@ -26,9 +28,19 @@ const Dropdown = ({ options, value, onChange }) => {
     });
 
     return (
-        <div>
-            <div onClick={handleClick}>{value?.label || "Select..."}</div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div className="w-48 relative">
+            <div
+                className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+                onClick={handleClick}
+            >
+                {value?.label || "Select..."}
+                <GoChevronDown className="text-lg" />
+            </div>
+            {isOpen && (
+                <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+                    {renderedOptions}
+                </div>
+            )}
         </div>
     );
 };
