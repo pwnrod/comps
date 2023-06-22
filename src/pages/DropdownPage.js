4cc36ed39c7991e05506d3c666f2199998f29621
Caylin James <cjames@leapagency.com>
2023-06-22 11:58:27 -0400

    create the DropdownPage component

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,22 @@
+import Dropdown from "../components/Dropdown";
+import { useState } from "react";
+
+const DropdownPage = () => {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
+    const options = [
+        { label: "Red", value: "red" },
+        { label: "Green", value: "green" },
+        { label: "Blue", value: "blue" },
+    ];
+
+    return (
+        <Dropdown onChange={handleSelect} value={selection} options={options} />
+    );
+};
+
+export default DropdownPage;
