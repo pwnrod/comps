37b46efeb026436681117ddf0034069c5c44088f
Caylin James <cjames@leapagency.com>
2023-06-22 12:24:48 -0400

    handle user clicking on Forward or Back buttons

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    // handle updating URL when user clicks the 'Back' or 'Forward' buttons
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+        window.addEventListener("popstate", handler);
+
+        return () => {
+            window.removeEventListener("popstate", handler);
+        };
+    }, []);
+
     return (
         <NavigationContext.Provider value={{}}>
+            {currentPath}
             {children}
         </NavigationContext.Provider>
     );
