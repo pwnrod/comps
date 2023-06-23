0972a1807866d279152e747096664388bc08fea4
Caylin James <cjames@leapagency.com>
2023-06-23 11:16:10 -0400

    fill the table with some data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,26 @@
-const Table = () => {
-    return <div>Table</div>;
+const Table = ({ data }) => {
+    let renderedRows = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>{renderedRows}</tbody>
+        </table>
+    );
 };
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,7 +1,14 @@
 import Table from "../components/Table";
 
 const TablePage = () => {
-    return <Table />;
+    const data = [
+        { name: "Orange", color: "bg-orange-500", score: 5 },
+        { name: "Apple", color: "bg-red-500", score: 3 },
+        { name: "Banana", color: "bg-yellow-500", score: 1 },
+        { name: "Lime", color: "bg-green-500", score: 4 },
+    ];
+
+    return <Table data={data} />;
 };
 
 export default TablePage;
