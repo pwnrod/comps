24bc66645d120da4dd461083ceffcbab3d392549
Caylin James <cjames@leapagency.com>
2023-06-23 09:32:56 -0400

    add props for the modal and set up button to close it

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -13,12 +13,26 @@ const ModalPage = () => {
         setShowModal(false);
     };
 
+    const actionBar = (
+        <div>
+            <Button primary onClick={handleClose}>
+                I Accept
+            </Button>
+        </div>
+    );
+
+    const modal = (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+            <p>Here is an important agreement for you to accept</p>
+        </Modal>
+    );
+
     return (
         <div>
             <Button onClick={handleClick} primary>
                 Open Modal
             </Button>
-            {showModal && <Modal onClose={handleClose} />}
+            {showModal && modal}
         </div>
     );
 };
