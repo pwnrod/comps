a438b378a5a58fe9ef07e33242654d6890fc97a5
Caylin James <cjames@leapagency.com>
2023-06-27 15:14:07 -0400

    add Immer to allow for 'mutable' state, which simplifies the approach

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -10,20 +11,25 @@ const ADD_VALUE_TO_COUNT = "add-value-to-count";
 const reducer = (state, action) => {
     switch (action.type) {
         case INCREMENT_COUNT:
-            return { ...state, count: state.count + 1 };
+            state.count = state.count + 1;
+            return;
         case DECREMENT_COUNT:
-            return { ...state, count: state.count - 1 };
+            state.count = state.count - 1;
+            return;
         case CHANGE_VALUE_TO_ADD:
-            return { ...state, valueToAdd: action.payload };
+            state.valueToAdd = action.payload;
+            return;
         case ADD_VALUE_TO_COUNT:
-            return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+            state.count = state.count + state.valueToAdd;
+            state.valuetoAdd = 0;
+            return;
         default:
             throw new Error("Unexpected action type: " + action.type);
     }
 };
 
 const CounterPage = ({ initialCount }) => {
-    const [state, dispatch] = useReducer(reducer, {
+    const [state, dispatch] = useReducer(produce(reducer), {
         count: initialCount,
         valueToAdd: 0,
     });
