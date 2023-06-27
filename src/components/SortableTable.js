2cbee9ae5c352777cb9ae8bc88622d42cc451739
Caylin James <cjames@leapagency.com>
2023-06-27 12:36:01 -0400

    get the table to always sort 'asc' when switching columns

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -8,6 +8,12 @@ const SortableTable = (props) => {
     const { config, data } = props;
 
     const handleClick = (label) => {
+        if (sortBy && label !== sortBy) {
+            setSortOrder("asc");
+            setSortBy(label);
+            return;
+        }
+
         if (sortOrder === null) {
             setSortOrder("asc");
             setSortBy(label);
@@ -60,12 +66,7 @@ const SortableTable = (props) => {
         });
     }
 
-    return (
-        <div>
-            {sortOrder} - {sortBy}
-            <Table {...props} data={sortedData} config={updatedConfig} />
-        </div>
-    );
+    return <Table {...props} data={sortedData} config={updatedConfig} />;
 };
 
 function getIcons(label, sortBy, sortOrder) {
